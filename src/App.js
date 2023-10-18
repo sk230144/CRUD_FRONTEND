a451372daeaca4cda398a51c035535ea963e2c68
hp <risabht043@gmail.com>
2023-10-18 05:28:16 -0400

    firsyt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,182 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
+import { MdClose } from 'react-icons/md'
+import axios from 'axios';
+import Formtable from './components/Formtable';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+axios.defaults.baseURL = "http://localhost:8080";
+
+const App = () => {
+
+  const [addSection, setAddSection] = useState(false)
+  const [formData, setFormData] = useState({
+    name : "",
+    email: "",
+    mobile: "",
+    status: "",
+  })
+  const [dataList,setDataList] = useState([])
+  const [editSection,setEditSection] = useState(false)
+  const [formDataEdit,setFormDataEdit] = useState({
+    name : "",
+    email : "",
+    mobile : "",
+    _id : ""
+  })
+
+
+
+
+const handleOnChange = (e)=>{
+  const {value,name} = e.target
+  setFormData((preve)=>{
+      return{
+        ...preve,
+        [name] : value
+      }
+  })
 }
 
-export default App;
+
+
+
+const handleSubmit = async(e)=>{
+  e.preventDefault()
+  e.preventDefault()
+  const data = await axios.post("/create",formData)
+  if(data.data.success){
+    setAddSection(false)
+    alert(data.data.message)
+  }
+}
+
+
+
+
+
+const getFetchData = async()=>{
+  const data = await axios.get("/")
+  console.log(data);
+  if(data.data.success){
+      setDataList(data.data.data)
+  }
+}
+useEffect(()=>{
+   getFetchData()
+}, [])
+
+
+
+
+
+
+const handleDelete = async(id) => {
+  const data = await axios.delete("/delete/"+id)
+  if(data.data.success){
+    getFetchData()
+    alert(data.data.message);
+  }
+}
+
+
+const handleUpdate = async(e)=>{
+  e.preventDefault()
+  const data = await axios.put("/update",formDataEdit)
+  if(data.data.success){
+    getFetchData()
+    alert(data.data.message)
+    setEditSection(false)
+  }
+}
+const handleEditOnChange = async(e)=>{
+  const {value,name} = e.target
+  setFormDataEdit((preve)=>{
+      return{
+        ...preve,
+        [name] : value
+      }
+  })
+}
+const handleEdit = (el)=>{
+  setFormDataEdit(el)
+  setEditSection(true)
+}
+
+
+  return (
+    <>
+    <div className="container">
+          <button className="btn btn-add" onClick={()=>setAddSection(true)}>Add</button>
+     {
+        addSection && (
+          <Formtable
+            handleSubmit={handleSubmit}
+            handleOnChange={handleOnChange}
+            handleclose = {()=>setAddSection(false)}
+            rest={formData}
+          />
+          )
+    }
+
+
+    {
+      editSection && (
+        <Formtable
+            handleSubmit={handleUpdate}
+            handleOnChange={handleEditOnChange}
+            handleclose = {()=>setEditSection(false)}
+            rest={formDataEdit}
+          />
+      )
+    }
+
+
+
+
+
+    <div className='tableContainer'>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Shift starts at</th>
+              <th>Shift ends at</th>
+              <th>
+              
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            { dataList[0] ? (
+              dataList.map((el)=>{
+                console.log(el)
+                return(
+                  <tr>
+                    <td>{el.name}</td>
+                    <td>{el.email}</td>
+                    <td>{el.mobile}</td>
+                    <td>{el.status}</td>
+                    <td>
+                    <button className='btn btn-edit' onClick={()=>handleEdit(el)}>Edit</button>
+                      <button className='btn btn-delete' onClick={()=>handleDelete(el._id)}>Delete</button>
+                    </td>
+                  </tr>
+                )
+              }))
+              : (
+                <p style={{textAlign : "center"}}>No data</p>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default App
+
